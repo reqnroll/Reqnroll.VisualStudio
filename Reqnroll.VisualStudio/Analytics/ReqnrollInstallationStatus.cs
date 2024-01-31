11d605912ea8f5d6530633ff0dfc5e0c8ffb2b51
Gaspar Nagy <gaspar.nagy@gmail.com>
2024-01-31 15:22:34 +0100

    change magic date

--- Reqnroll.VisualStudio/Analytics/ReqnrollInstallationStatus.cs
+++ Reqnroll.VisualStudio/Analytics/ReqnrollInstallationStatus.cs
@@ -2,7 +2,7 @@ namespace Reqnroll.VisualStudio.Analytics;
 
 public class ReqnrollInstallationStatus
 {
-    public static readonly DateTime MagicDate = new(2009, 9, 11); //when Reqnroll has born
+    public static readonly DateTime MagicDate = new(2024, 1, 12); // when Reqnroll has born
     public static readonly Version UnknownVersion = new(0, 0);
     public bool IsInstalled => InstalledVersion != UnknownVersion;
     public Version InstalledVersion { get; set; } = UnknownVersion;
