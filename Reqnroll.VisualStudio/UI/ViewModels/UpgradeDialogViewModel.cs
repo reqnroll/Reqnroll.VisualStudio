534ebc36df6c0d6f801e50058d56b3fabc842856
Gáspár Nagy <gaspar.nagy@gmail.com>
2024-06-05 12:25:28 +0200

    fix build

--- Reqnroll.VisualStudio/UI/ViewModels/UpgradeDialogViewModel.cs
+++ Reqnroll.VisualStudio/UI/ViewModels/UpgradeDialogViewModel.cs
@@ -30,15 +30,13 @@ public class UpgradeDialogViewModel : WizardViewModel
 
 #if DEBUG
     public static UpgradeDialogViewModel DesignData = new("1.0.99",
-        """
-        # v1.0.1 - 2019-02-27
+        @"# v1.0.1 - 2019-02-27
 
-        ## Bug fixes:
+## Bug fixes:
 
-        * CreatePersistentTrackingPosition Exception / Step navigation error (#3)
-        * .NET Core Bindings: Unable to load BoDi.dll (temporary fix)
-
-        """);
+* CreatePersistentTrackingPosition Exception / Step navigation error (#3)
+* .NET Core Bindings: Unable to load BoDi.dll (temporary fix)
+");
 #endif
 
     public UpgradeDialogViewModel(string newVersion, string changeLog) : base("Close", "Welcome to Reqnroll",
