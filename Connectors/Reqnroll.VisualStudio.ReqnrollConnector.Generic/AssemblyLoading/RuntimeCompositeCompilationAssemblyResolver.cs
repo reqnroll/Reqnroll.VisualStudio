50004087e4039bbc8bebb167f0c065974a3a2bac
Gáspár Nagy <gaspar.nagy@gmail.com>
2025-01-22 15:54:38 +0100

    fix net462 discovery

--- Connectors/Reqnroll.VisualStudio.ReqnrollConnector.Generic/AssemblyLoading/RuntimeCompositeCompilationAssemblyResolver.cs
+++ Connectors/Reqnroll.VisualStudio.ReqnrollConnector.Generic/AssemblyLoading/RuntimeCompositeCompilationAssemblyResolver.cs
@@ -11,15 +11,22 @@ public class RuntimeCompositeCompilationAssemblyResolver : ICompilationAssemblyR
         _log = log;
     }
 
-    public bool TryResolveAssemblyPaths(CompilationLibrary library, List<string> assemblies)
+    public bool TryResolveAssemblyPaths(CompilationLibrary library, List<string>? assemblies)
     {
         foreach (ICompilationAssemblyResolver resolver in _resolvers)
             try
             {
                 if (resolver.TryResolveAssemblyPaths(library, assemblies) &&
+                    assemblies != null &&
                     assemblies.Any(a => !IsRefsPath(a)))
                 {
-                    var resolveAssemblyPath = assemblies.FirstOrDefault(a => !IsRefsPath(a));
+                    var resolveAssemblyPath = assemblies.First(a => !IsRefsPath(a));
+                    if ("System.Runtime.dll".Equals(Path.GetFileName(resolveAssemblyPath), StringComparison.OrdinalIgnoreCase))
+                    {
+                        _log.Info($"Skipping {resolveAssemblyPath} ({resolver})");
+                        assemblies.Remove(resolveAssemblyPath);
+                        return false;
+                    }
                     _log.Info($"Resolved with {resolver} from {resolveAssemblyPath}");
                     return true;
                 }
