30f1ce5e348680c4728d70baa4e4e1b5f664851b
Chris Rudolphi <1702962+clrudolphi@users.noreply.github.com>
2025-12-17 09:41:04 -0600

    Feature generate async snippets from 'DefineSteps' command (#129)
    * Snippet generator honors the StepDefinitionSkeletonStyle setting from the reqnroll.json file when generating snippets (sync or async).
    Added unit tests for the new skeletonStyle enum deserialization.
    Added unit test for the SnippetService.
    Added Spec test in the DefineStepsCommand spec for the generation of async and sync snippets.
    
    * Updated Changelog
    
    * Add PR number to CHANGELOG
    
    ---------
    
    Co-authored-by: Gáspár Nagy <gaspar.nagy@gmail.com>

--- Reqnroll.VisualStudio/Snippets/SnippetExpressionStyle.cs
+++ Reqnroll.VisualStudio/Snippets/SnippetExpressionStyle.cs
@@ -3,5 +3,26 @@ namespace Reqnroll.VisualStudio.Snippets;
 public enum SnippetExpressionStyle
 {
     RegularExpression,
-    CucumberExpression
+    CucumberExpression,
+    AsyncRegularExpression,
+    AsyncCucumberExpression
+}
+
+public static class SnippetExpressionStyleExtensions
+{
+    public static bool IsAsync(this SnippetExpressionStyle style)
+    {
+        if (style == SnippetExpressionStyle.AsyncRegularExpression
+            || style == SnippetExpressionStyle.AsyncCucumberExpression)
+            return true;
+        return false;
+    }
+
+    public static bool IsCucumber(this SnippetExpressionStyle style)
+    {
+        if (style == SnippetExpressionStyle.CucumberExpression
+            || style == SnippetExpressionStyle.AsyncCucumberExpression)
+            return true;
+        return false;
+    }
 }
