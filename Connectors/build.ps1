3dc4f1d1edffd5d322f95c8893b5fdf2f52704e1
Gáspár Nagy <gaspar.nagy@gmail.com>
2026-01-19 14:55:11 +0100

    Add Reqnroll v1 connectors as pre-built instead of building them

--- Connectors/build.ps1
+++ Connectors/build.ps1
@@ -16,28 +16,6 @@ Remove-Item $outputFolder -Recurse -Force -ErrorAction SilentlyContinue
 
 mkdir $outputFolder
 
-# build Reqnroll V1 any cpu
-
-cd $PSScriptRoot\Reqnroll.VisualStudio.ReqnrollConnector.V1
-
-dotnet publish $buildArgs
-
-mkdir $outputFolder\Reqnroll-V1\
-Copy-Item bin\$configuration\net48\publish\* $outputFolder\Reqnroll-V1\ -Exclude @('System.*', 'Gherkin.*','*.exe.config')
-
-# build Reqnroll V1 x86
-
-Remove-Item bin\$configuration\net48\win-x86\publish -Recurse -Force -ErrorAction SilentlyContinue
-
-dotnet publish -r win-x86 $buildArgs /p:PlatformTarget=x86
-
-Rename-Item bin\$configuration\net48\win-x86\publish\reqnroll-vs.exe reqnroll-vs-x86.exe -Force
-Rename-Item bin\$configuration\net48\win-x86\publish\reqnroll-vs.pdb reqnroll-vs-x86.pdb -Force
-
-Copy-Item bin\$configuration\net48\win-x86\publish\reqnroll-vs-x86.* $outputFolder\Reqnroll-V1\
-
-cd ..
-
 # build Reqnroll generic any cpu
 pushd
 cd Reqnroll.VisualStudio.ReqnrollConnector.Generic
