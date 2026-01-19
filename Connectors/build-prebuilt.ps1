87f1a78e7fc0c57fd538e40eb083b9e10db05c27
Gáspár Nagy <gaspar.nagy@gmail.com>
2026-01-19 14:03:12 +0100

    test net 10 build (#152)
    * test net 10 build
    
    * try to use built .net 10 connector

--- Connectors/build-prebuilt.ps1
+++ Connectors/build-prebuilt.ps1
@@ -12,16 +12,4 @@ if ($additionalArgs -ne '-') {
 
 Write-Output "ARGS: $buildArgs"
 
-Remove-Item $outputFolder -Recurse -Force -ErrorAction SilentlyContinue
-
-mkdir $outputFolder
-
-# build Reqnroll generic .NET 10.0
-pushd
-cd Reqnroll.VisualStudio.ReqnrollConnector.Generic
-
-dotnet publish -f net10.0 -p CustomConnectorFrameworks="net10.0" $buildArgs
-
-Copy-Item bin\$configuration\net10.0\publish\ $outputFolder\Reqnroll-Generic-net10.0\ -Recurse
-
-popd
+Write-Output "There is currently nothing to pre-build"
