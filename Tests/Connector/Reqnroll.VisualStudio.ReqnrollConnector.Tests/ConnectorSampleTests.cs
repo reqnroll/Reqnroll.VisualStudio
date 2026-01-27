40d6f636260d3a2646c57720d301c104f22d7981
Gáspár Nagy <gaspar.nagy@gmail.com>
2026-01-27 19:51:04 +0100

    Add connector tests (#158)
    * Add Tests\VsExtConnectorTestSamples to the connector tests
    
    * add Calculator sample

---
+++ Tests/Connector/Reqnroll.VisualStudio.ReqnrollConnector.Tests/ConnectorSampleTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Reqnroll.VisualStudio.ReqnrollConnector.Tests;
+
+public class ConnectorSampleTests : SampleProjectTestBase
+{
+    public ConnectorSampleTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+    {
+    }
+
+    [Theory]
+    [MemberData(nameof(GetProjectsForRepository), @"Tests\VsExtConnectorTestSamples", "")]
+    public void All(string testCase, string projectFile, string repositoryDirectory)
+    {
+        ValidateProject(testCase, projectFile, repositoryDirectory);
+    }
+}
